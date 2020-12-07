fix timing in svrg and gradient update timing of lsvrg

algorithms.py
@@ -28,9 +28,12 @@ def pnp_svrg(params, denoiser, eta, T1, T2, mini_batch_size, verbose=True):
 
             # calculate stochastic variance-reduced gradient (SVRG)
             v = grad(z, ind, params['y'], params['F'], params['H']) - grad(w, ind, params['y'], params['F'], params['H']) + mu
-            
+
             # Gradient update
             z -= (eta*params['lr_decay']**params['t'])*v
+
+            if verbose:
+                print("After gradient update: " + str(i) + " " + str(j) + " " + str(peak_signal_noise_ratio(params['original'], z)))
 
             # Denoise
             z = denoise(kind=denoiser, noisy=z, params=params)
@@ -45,7 +48,7 @@ def pnp_svrg(params, denoiser, eta, T1, T2, mini_batch_size, verbose=True):
             psnr_per_iter.append(peak_signal_noise_ratio(params['original'], z))
 
             if verbose:
-                print(str(i) + " " + str(j) + " " + str(psnr_per_iter[-1]))
+                print("After denoising update: " + str(i) + " " + str(j) + " " + str(psnr_per_iter[-1]))
 
     # output denoised image, time stats, psnr stats
     return z, time_per_iter, psnr_per_iter, zs
@@ -145,12 +148,18 @@ def pnp_lsvrg(params, denoiser, eta, T, mini_batch_size, prob_update=0.1, verbos
 
     w = np.copy(z)
 
+    start_time = time.time()
+
+    # calculate full gradient
+    mu = grad(z, params['mask'], params['y'], params['F'], params['H'])
+
+    time_per_iter.append(time.time() - start_time)
+
+    psnr_per_iter.append(peak_signal_noise_ratio(params['original'], z))
+
     for i in range(T):
         # start timing
         start_time = time.time()
-
-        # calculate full gradient
-        mu = grad(z, params['mask'], params['y'], params['F'], params['H'])
 
         # Get minibatch
         ind = get_batch(mini_batch_size, params['mask'])
@@ -171,6 +180,7 @@ def pnp_lsvrg(params, denoiser, eta, T, mini_batch_size, prob_update=0.1, verbos
         # update reference point with probability prob_update
         if np.random.random() < prob_update:
             w = np.copy(z)
+            mu = grad(z, params['mask'], params['y'], params['F'], params['H'])
 
         # Log timing
         time_per_iter.append(time.time() - start_time)