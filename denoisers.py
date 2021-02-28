7d0af51c2b7d83532f77ab4a9ff9af97f7d8f2e4
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2021-02-28 00:06:02 -0800

    New denoisers.py and denoise.ipynb to reflect new Problem instance

--- denoisers.py
+++ denoisers.py
@@ -1,14 +1,11 @@
 from imports import *
 
-
-def denoise(kind, noisy, params):
+def denoise(kind, noisy, problem):
     if kind == 'cnn':
-        return (noisy - (params['cnn_decay'] ** params['t']) * params['cnn'](
-            torch.Tensor(noisy)[None][None].to(params['device'])).squeeze().detach().cpu().numpy())
+        return (noisy - (problem.params['cnn_decay']**problem.params['t'])*problem.params['cnn'](torch.Tensor(noisy)[None][None].to(problem.params['device'])).squeeze().detach().cpu().numpy())
     elif kind == 'nlm':
-        return denoise_nl_means(noisy, h=params['filter'] * params['filter_decay'] ** params['t'], fast_mode=True,
-                                **params['patch'])
+        return denoise_nl_means(noisy, h=problem.params['filter']*problem.params['filter_decay']**problem.params['t'], fast_mode=True, **problem.params['patch'])
     elif kind == 'tv':
-        return denoise_wavelet(noisy, multichannel=params['multia'], rescale_sigma=params['rescale_sigma'])
+        return denoise_wavelet(noisy, multichannel=problem.params['multia'], rescale_sigma=problem.params['rescale_sigma'])
     elif kind == 'bm3d':
-        return bm3d(noisy, params['noise_est'] * params['filter_decay'] ** params['t'])
+        return bm3d(noisy, problem.params['noise_est']*problem.params['filter_decay']**problem.params['t'])
