fa749d1c541f66da6a1477a249101e9dede35f19
vmonardo <vince.monardo@gmail.com>
2021-06-15 15:25:34 -0400

    Update ICIP 2021 Final Figures

--- algorithms.py
+++ algorithms.py
@@ -37,7 +37,7 @@ def pnp_svrg(problem, denoiser, eta, tt, T2, mini_batch_size, verbose=True):
             start_time = time.time()
 
             # calculate stochastic variance-reduced gradient (SVRG)
-            v = problem.stoch_grad(z, mini_batch_size) - problem.stoch_grad(w, mini_batch_size) + mu
+            v = (problem.stoch_grad(z, mini_batch_size) - problem.stoch_grad(w, mini_batch_size)) / mini_batch_size + mu
 
             # Gradient update
             z -= (eta*problem.lr_decay**denoiser.t)*v
