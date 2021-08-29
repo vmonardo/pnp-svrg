fix factor of 2 inconsistency

algorithms/pnp_svrg.py
@@ -33,7 +33,7 @@ def pnp_svrg(problem, denoiser, eta, tt, T2, mini_batch_size, verbose=True, lr_d
         w = np.copy(z) 
 
         time_per_iter.append(time.time() - start_time)
-        psnr_per_iter.append(peak_signal_noise_ratio(problem.X, z))
+        psnr_per_iter.append(peak_signal_noise_ratio(problem.X.reshape(problem.H,problem.W), z.reshape(problem.H,problem.W)))
 
         # inner loop
         for j in range(T2): 
@@ -51,7 +51,8 @@ def pnp_svrg(problem, denoiser, eta, tt, T2, mini_batch_size, verbose=True, lr_d
             v = (problem.grad_stoch(z, mini_batch) - problem.grad_stoch(w, mini_batch)) / mini_batch_size + mu
 
             # Gradient update
-            z -= (eta*lr_decay**denoiser.t)*v
+            z = z.reshape(problem.H,problem.W)
+            z -= (eta*lr_decay**denoiser.t)*v.reshape(problem.H,problem.W)
 
             # end gradient timing
             grad_end_time = time.time() - grad_start_time

problems/CSMRI.py
@@ -32,7 +32,7 @@ class CSMRI(Problem):
 
         # maintaining consistency for debugging
         self.lrH, self.lrW = self.H, self.W     
-        self.M = self.N
+        self.M = np.count_nonzero(self.mask)
 
     def _generate_mask(self):
         # Generate random binary mask to determine sampled Fourier coefficients
@@ -55,7 +55,7 @@ class CSMRI(Problem):
     def f(self, w):
         # f(W) = 1 / 2*M || Y - M o F{W} ||_F^2
         # Compute data fidelity function value at a given point
-        return np.linalg.norm(self.Y - self.forward_model(w)) ** 2 / 2 / self.H / self.W
+        return np.linalg.norm(self.Y - self.forward_model(w)) ** 2 / 2 / self.M
 
     def grad_full(self, z):
         # Get objects as images
@@ -69,7 +69,7 @@ class CSMRI(Problem):
         # return inverse 2D Fourier Transform of residual
         # tmp = np.real(np.fft.ifft2(res))
         tmp = np.real(np.conj(self.F) @ res @ np.conj(self.F.T)) 
-        return tmp * 2 / self.H / self.W
+        return tmp / self.M
 
     def grad_stoch(self, z, mb):
         # Get objects as images
@@ -90,7 +90,7 @@ class CSMRI(Problem):
 
         # return inverse 2D Fourier Transform of residual
         op = np.real(np.einsum('ij,ik->jk',tmp,np.conj(F_j)))
-        return op * 2 / self.H / self.W
+        return op
 
 # use this for debugging
 if __name__ == '__main__':

problems/problem.py
@@ -50,13 +50,14 @@ class Problem():
     def grad_full_check(self):
         # Check the gradient implementation at a random value
         w = np.random.uniform(0.0, 1.0, self.N)
+        
         delta = np.zeros(self.N)
         grad = np.zeros(self.N)
         eps = 1e-6
 
         for i in range(self.N):
             delta[i] = eps
-            grad[i] = (self.f(w+delta) -  self.f(w - delta)) / eps   
+            grad[i] = (self.f(w+delta) -  self.f(w)) / eps   
             delta[i] = 0
 
         grad_comp = self.grad_full(w).flatten()
@@ -71,17 +72,17 @@ class Problem():
             return True
         
     def grad_stoch_check(self):
-        # check that (grad) f(w) = sum((grad) f_i(w)) at a random value
+        # check that (grad) f(w) = sum((grad) f_i(w)) / m at a random value
         w = np.random.uniform(0.0, 1.0, self.N)
         full_grad = self.grad_full(w)
         grad_comp = np.zeros(self.N)
 
-        for i in range(self.M):
-            mb = np.zeros(self.M, dtype=int)
+        for i in range(self.N):
+            mb = np.zeros(self.N, dtype=int)
             mb[i] = 1
             grad_comp += self.grad_stoch(w, mb).flatten()
 
-        if np.linalg.norm(full_grad.flatten() - grad_comp) > 1e-6:
+        if np.linalg.norm(full_grad.flatten() - grad_comp / self.M) > 1e-6:
             print('Stoch Grad check failed!')
             print('full grad: ', full_grad)
             print('grad comp: ', grad_comp)